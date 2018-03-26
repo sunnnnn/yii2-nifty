c33fc37a295f0006661a7b194430c4dae95a468d
姚立 <263021000@qq.com>
2018-03-26 15:47:39 +0800

    add widget

--- basic/views/site/error.php
+++ basic/views/site/error.php
@@ -4,17 +4,18 @@ $this->registerJs('$(function(){$(".default-page-head").remove();$(".default-pag
 $this->title = $name;
 ?>
 <div id="page-head">
+	<h3 class="panel-title"><?= Html::a('<i class="pli-left-4"></i> '.Yii::t('app/view', 'back'), 'javascript:history.back();', ['class' => 'btn btn-mint']); ?></h3>
     <div class="text-center cls-content">
-        <h1 class="error-code text-info" style="font-size: 60px;"><?= Yii::t('app', 'error_page'); ?></h1>
+        <h1 class="error-code text-info" style="font-size: 60px;"><i class="pli-depression"></i></h1>
     </div>
 </div>
 
 <div id="page-content">
 
 	<div class="text-center cls-content" style="padding-top: 50px;">
-	    <p class="h4 text-uppercase text-bold"><?= Html::encode($this->title) ?></p>
+	    <p class="h4 text-uppercase text-bold"><?= nl2br(Html::encode($message)) ?></p>
 	    <div class="pad-btm">
-	        <?= nl2br(Html::encode($message)) ?>
+	        <?= Html::encode($this->title)?>
 	    </div>
 	</div>
 
